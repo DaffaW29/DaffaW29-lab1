f8f7b3795b7979c9da93b3c9ac553d2023e2df88
Daffa Warsa <daffawarsa@Daffas-MacBook-Pro.local>
2024-09-23 15:17:43 -0400

    Plan/layout

--- Enigma.java
+++ Enigma.java
@@ -10,7 +10,7 @@ public class Enigma{
     private Rotor rotors[];
         
     public Enigma(int id1, int id2, int id3, String start){
-
+        //looks like the rotor 0 is the inside, rotor 1 is middle, rotor 2 is the outside
         rotors = new Rotor[3];
         rotors[0] = new Rotor(rotorInit[id1-1], start.charAt(0));
         rotors[1] = new Rotor(rotorInit[id2-1], start.charAt(1));
@@ -21,13 +21,18 @@ public class Enigma{
 
     public String decrypt(String message){        
         //TODO
+        //plan is to 1) get the index of the first char top
+        //2) get the char from that same index from middle
+        //3) take get char and find the index of it from the outer
+        //4) get the index and use it to get the char from the inner
+        //5) determine whatever character is in the second and call the rotate to that
     }
 
 
     
     public String encrypt(String message){
         //TODO
-
+    }
     
     private void rotate(){
         if(rotors[0].rotate()){
