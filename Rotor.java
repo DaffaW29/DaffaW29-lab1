58656e56e62234cde3a10d9c054e50848219b7ef
Daffa Warsa <daffawarsa@daffas-mbp.myfiosgateway.com>
2024-09-24 23:12:06 -0400

    Finalizing the rotation

--- Rotor.java
+++ Rotor.java
@@ -18,7 +18,8 @@ public class Rotor {
         // so im trying to figure out how to utilize the indexOf method
         //what I think is that we can indexOf the startchar then count till we get there
         // that way we dont need to compare every time. but i think both work
-        this.rotorValues = this.rotorValues.substring(1,27) + this.rotorValues.charAt(0);
+        //also realized that i was rotating wrong, now it moves the end to the front as opposed to the front to the end
+        this.rotorValues = this.rotorValues.charAt(26) + this.rotorValues.substring(0,26);
         if (this.rotorValues.charAt(0) == this.startChar) {
             return true;
         } else {
